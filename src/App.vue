c695587a2b932ff25f40e62e2c87a0ee6dd91269
Roberto Treviño Cervantes <rtrevinnoc@hotmail.com>
2022-11-21 03:14:37 -0600

    p value

--- src/App.vue
+++ src/App.vue
@@ -11,27 +11,29 @@
     <br>
     <table>
       <tr>
-          <td>Source of Variation</td>
-          <td>Sum of Squares</td>
-          <td>df</td>
-          <td>Mean Square</td>
-          <td>F</td>
+        <td>Source of Variation</td>
+        <td>Sum of Squares</td>
+        <td>df</td>
+        <td>Mean Square</td>
+        <td>F</td>
+        <td>P</td>
       </tr>
-          <td>Regression (explained)</td>
-          <td>2920.68100</td>
-          <td>{{ this.dfb }}</td>
-          <td>2920.68100</td>
-          <td>{{ this.f_calc }}</td>
+        <td>Regression (explained)</td>
+        <td>{{ this.SSb }}</td>
+        <td>{{ this.dfb }}</td>
+        <td>{{ this.Sqb }}</td>
+        <td>{{ this.f_calc }}</td>
+        <td>{{ this.f_table }}</td>
       <tr>
-          <td>Residual (unexplained)</td>
-          <td>4342.38800</td>
-          <td>{{ this.dfw }}</td>
-          <td>542.79850</td>
+        <td>Residual (unexplained)</td>
+        <td>{{ this.SSw }}</td>
+        <td>{{ this.dfw }}</td>
+        <td>{{ this.Sqw }}</td>
       </tr>
       <tr>
-          <td>Total</td>
-          <td>{{ this.SSt }}</td>
-          <td>{{ this.dft }}</td>
+        <td>Total</td>
+        <td>{{ this.SSt }}</td>
+        <td>{{ this.dft }}</td>
       </tr>
     </table>
     <br>
@@ -44,11 +46,6 @@
 </template>
 
 <script>
-// import fdistr from '@/statistics-distributions'
-// import { create, all } from "mathjs";
-
-// const math = create(all, {});
-
 Number.prototype.round = function(n) {
   const d = Math.pow(10, n);
   return Math.round((this + Number.EPSILON) * d) / d;
@@ -99,6 +96,25 @@ export default {
         list.pop();
       });
     },
+    fhelper(t,r,a,e) {
+      for (var n = 1, s = n, d = r; a >= d;) n = n * t * d / (d - e), s += n, d += 2;
+      return s
+    },
+    fdist(t,r,a) {
+      var e = a / (r * t + a);
+      if (r % 2 == 0) return this.fhelper(1 - e, a, r + a - 4, a - 2) * Math.pow(e, a / 2);
+      if (a % 2 == 0) return 1 - this.fhelper(e, r, r + a - 4, r - 2) * Math.pow(1 - e, r / 2);
+      var n = Math.atan(Math.sqrt(r * t / a)),
+          s = n / (Math.PI / 2),
+          d = Math.cos(n),
+          o = Math.sin(n);
+      if (a > 1 && (s += d * o * this.fhelper(d * d, 2, a - 3, -1) / (Math.PI / 2)), 1 == r) return 1 - s;
+      var l = 4 * this.fhelper(o * o, a + 1, r + a - 4, a - 2) * o * Math.pow(d, a) / Math.PI;
+      if (1 == a) return 1 - s + l / 2;
+      for (var h = 2;
+          (a - 1) / 2 >= h;) l = l * h / (h - .5), h += 1;
+      return 1 - s + l
+    },
     solve() {
       this.n = (this.n_data + 1) * (this.n_trat + 1);
       this.dfb = (this.n_data + 1) - 1;
@@ -108,10 +124,15 @@ export default {
       this.avg = (this.avgs.reduce((acc, x) => acc + x) / this.avgs.length).round(3)
       this.SSt = this.fs[0].map((col, i) => this.fs.map(row => row[i]).reduce((acc, c) => acc + ((c - this.avg) ** 2), 0)).reduce((acc, y) => acc + y).round(3);
       this.SSw = this.fs[0].map((col, i) => this.fs.map(row => row[i]).reduce((acc, c) => acc + ((c - this.avgs[i]) ** 2), 0)).reduce((acc, y) => acc + y).round(3);
-      this.SSb = this.SSt - this.SSw
-      this.Sqb = this.SSb/this.dfb
-      this.Sqw = this.SSw/this.dfw
+      this.SSb = (this.SSt - this.SSw).round(3)
+      this.Sqb = (this.SSb/this.dfb).round(3)
+      this.Sqw = (this.SSw/this.dfw).round(3)
       this.f_calc = (this.Sqb/this.Sqw).round(3)
+      this.f_table = this.fdist(this.f_calc, this.dfb, this.dfw).round(3)
+
+      console.log(this.SSb, this.SSw, this.SSt);
+      console.log(this.dfb, this.dfw, this.dft);
+      console.log(this.f_table)
     }
   },
 };
